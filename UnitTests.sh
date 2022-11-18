8e8aaa680078ca1acf414978722bdc9497f2fbc1
IonaHiggins <ihiggi200@caledonian.ac.uk>
2022-11-18 17:42:17 +0000

    Added test

--- UnitTests.sh
+++ UnitTests.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 test=`java CurrencyConverter 50 dollars`;
 echo ="$test";
+
+expectedOutput="=50.0 Dollars = 37 Pounds
+50.0 Dollars = 44 Euros"
+
+if [ "$expectedOutput" == "$test" ]
+then
+	echo "Dollars test successful";
+else:
+	echo "A maths issue has occured";
+fi
