f5ad2e4a864c306a7649b0ae3d194c0d99d7c2e9
IonaHiggins <ihiggi200@caledonian.ac.uk>
2022-11-18 21:15:28 +0000

    Changed System.out.println to log

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -20,26 +20,26 @@ public class CurrencyConverter {
                         case "dollars" :
                                 // For Dollar Conversion
                                 pound = amount * 0.74;
-                                System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
+                                logger.log(amount + " Dollars = " + f.format(pound) + " Pounds");
                                 euro = amount * 0.88;
-                                System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
+                                logger.log(amount + " Dollars = " + f.format(euro) + " Euros");
                                 break;
                         case "pounds":
                                 // For Pound Conversion
                                 dollar = amount * 1.36;
-                                System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
+                                logger.log(amount + " Pounds = " + f.format(dollar) + " Dollars");
                                 euro = amount * 1.19;
-                                System.out.println(amount + " Pound = " + f.format(euro) + " Euros");
+                                logger.log(amount + " Pound = " + f.format(euro) + " Euros");
                                 break;
                         case "euros":
                                 // For Euro Conversion
                                 dollar = amount * 1.13;
-                                System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
+                                logger.log(amount + " Euros = " + f.format(dollar) + " Dollars");
                                 pound = amount * 0.84;
-                                System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
+                                logger.log(amount + " Euros = " + f.format(pound) + " Pounds");
                                 break;
                 }
-                System.out.println("Thank you for using the converter.");
+                logger.log("Thank you for using the converter.");
         }
 
 }
