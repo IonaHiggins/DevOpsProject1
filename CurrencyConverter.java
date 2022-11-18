fb8ed167e645373cccb3b3ce28e7cbd5a7f27eea
IonaHiggins <ihiggi200@caledonian.ac.uk>
2022-11-18 22:52:08 +0000

    Re-instated original code and made small adjustments

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,49 +1,49 @@
-import java.util.*
+import java.util.*;
 import java.text.DecimalFormat;
 
-public class CurrencyConverter{
-	
-	public static void main(String[] args){
-	double amount;
-	double dollar;
-	double pound;
-	double euro;
+public class CurrencyConverter {
 
-	DecimalFormat f = new DecimalFormat("##.##");
+        public static void main(String[] args) {
+
+                double amount;
+		double dollar;
+		double pound;
+		double euro;
+
+                DecimalFormat f = new DecimalFormat("##.##");
+
+                amount = Integer.parseInt(args[0]);
+                String currency = args[1];
+
+                // For amounts Conversion
+                switch (currency){
+                        case "dollars" :
+                                // For Dollar Conversion
+                                pound = amount * 0.74;
+                                System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
+                                euro = amount * 0.88;
+                                System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
+                                break;
+                        case "pounds":
+                                // For Pound Conversion
+                                dollar = amount * 1.36;
+                                System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
+                                euro = amount * 1.19;
+                                System.out.println(amount + " Pound = " + f.format(euro) + " Euros");
+                                break;
+                        case "euros":
+                                // For Euro Conversion
+                                dollar = amount * 1.13;
+                                System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
+                                pound = amount * 0.84;
+                                System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
+                                break;
+                }
+                System.out.println("Thank you for using the converter.");
+        }
+
+}
 	
-	amount = int.parseInt(args[0]);
-	String currency = args[1];
-		
-	//For amounts Conversion
-	switch (currency){
-		case "dollars":
-		//For Dollar Conversion
-			pound = (amount * 0.74);
-			logger.log(amount + " Dollars = " + f.format(pound) + " Pounds");
-			euro = (amount * 0.88);
-			logger.log(amount + " Dollars = " + f.format(euro) + " Euros");
-			break;
-		case "pounds":
-			//For Pound Conversion
-			dollar = (amount * 1.36);
-			logger.log(amount + " Pounds = " + f.format(dollar) + " Dollars");
-			euro = (amount * 1.19);
-			logger.log(amount + " Pounds = " + f.format(euro) + " Euros");
-			break;
-		case "euros":
-			//For Euro Conversion
-			dollar = (amount * 1.13);
-			logger.log(amount + " Euros = " + f.format(dollar) + " Dollars");
-			pound = (amount * 0.84);
-			logger.log(amount + " Euros = " + f.format(pound) + " Pounds");		
-			break;
-		default: 
-			error("An error has occured");
-			break;
-		}
-	logger.log("Thank you for using the converter.");
-	}
-}	
 
 
 
