530ea15b30408b2931ec210c9d23b45968200f30
IonaHiggins <ihiggi200@caledonian.ac.uk>
2022-11-18 18:50:42 +0000

    Added tolerancy for  currency type test

--- UnitTests.sh
+++ UnitTests.sh
@@ -5,12 +5,30 @@ expectedOutput=$"50.0 Dollars = 37 Pounds
 50.0 Dollars = 44 Euros
 Thank you for using the converter.";
 
-echo "Testing false inputs"
+echo "Testing false inputs";
 
 if [ "$expectedOutput" == "$test" ]
 then
-	echo "test successful";
+	echo "Input recieved.";
+	echo "Test Passed";
 else
-	echo "An error has occured - unexpected input";
+	echo "An error occured - unexpected input";
+	echo "Test Failed";
 	exit 1;
+fi
+
+echo "Testing input variation";
+
+input ="DOLLARS";
+echo "$input";
+
+if [ "$input" == *"$dollars"* ] || [ "$input"  == *"$Dollars"* ] || [ "$input" == *"DOLLARS"* ] || [ "$input" == *"pounds"* ] ||  [ "$input" == *"Pounds"* ] || [ "$input" == *"POUNDS"* ] || [ "$input" == *"euros"* ] || [ "$input" == *"Euros"* ] || [ "$input" == *"EUROS"* ]
+
+then
+	echo "Valid currency type";
+	echo "Test Passed";
+else
+	echo "An error occured - unexpected input";
+	echo "Test Failed";
+	exit 2;
 fi
