613a7bfdc3f5f5b67eb663a83d0520953df598cd
IonaHiggins <ihiggi200@caledonian.ac.uk>
2022-11-18 18:26:32 +0000

    Using just spaces

--- UnitTests.sh
+++ UnitTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test=`java CurrencyConverter 50 dollars`;
 
-expectedOutput=$"50.0 Dollars = 37 Pounds\r\n50.0 Dollars = 44 Euros\r\nThank you for using the converter.";
+expectedOutput=$"50.0 Dollars = 37 Pounds 50.0 Dollars = 44 Euros Thank you for using the converter.";
 
 echo "Testing false inputs"
 
