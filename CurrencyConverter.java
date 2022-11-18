4378fb6970cb1b30d4a5db8e1e403f5f9ccd856c
IonaHiggins <ihiggi200@caledonian.ac.uk>
2022-11-18 22:46:33 +0000

    Added parenthesis

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -19,27 +19,26 @@ public class CurrencyConverter{
 		case "dollars":
 		//For Dollar Conversion
 			pound = (amount * 0.74);
-			logger.log(amount + " Dollars = " + f.format(pound) + " Pounds";
+			logger.log(amount + " Dollars = " + f.format(pound) + " Pounds");
 			euro = (amount * 0.88);
-			logger.log(amount + " Dollars = " + f.format(euro) + " Euros";
+			logger.log(amount + " Dollars = " + f.format(euro) + " Euros");
 			break;
 		case "pounds":
 			//For Pound Conversion
 			dollar = (amount * 1.36);
-			logger.log(amount + " Pounds = " + f.format(dollar) + " Dollars";
+			logger.log(amount + " Pounds = " + f.format(dollar) + " Dollars");
 			euro = (amount * 1.19);
-			logger.log(amount + " Pounds = " + f.format(euro) + " Euros";
+			logger.log(amount + " Pounds = " + f.format(euro) + " Euros");
 			break;
 		case "euros":
 			//For Euro Conversion
 			dollar = (amount * 1.13);
-			logger.log(amount + " Euros = " + f.format(dollar) + " Dollars";
+			logger.log(amount + " Euros = " + f.format(dollar) + " Dollars");
 			pound = (amount * 0.84);
-			logger.log(amount + " Euros = " + f.format(pound) + " Pounds";
-			logger.log(1.13*50);		
+			logger.log(amount + " Euros = " + f.format(pound) + " Pounds");		
 			break;
 		default: 
-			error("An error has occured);
+			error("An error has occured");
 			break;
 		}
 	logger.log("Thank you for using the converter.");
